704b52ed97b505b12a19115b2d2126a9beeee7e1
Lari-NC <cardozo.lara.edu@gmail.com>
2025-05-03 19:06:33 -0300

    edición con mock

--- frontend/src/components/Perfil/Perfil.jsx
+++ frontend/src/components/Perfil/Perfil.jsx
@@ -3,18 +3,26 @@ import { AuthContext } from '../../context/AuthContext'
 import { useNavigate, useParams } from 'react-router-dom'
 import WhatsApp from '../../assets/WhatsApp.png'
 import './Perfil.css'
+import editarPerfil from '../../service/editarPerfil'
 
 const Perfil = () => {
   const { matricula } = useParams()
   const [medico, setMedico] = useState({})
   const [error, setError] = useState(null)
-
+  const [editable, setEditable] = useState(false)
   const { logout } = useContext(AuthContext)
   const navigate = useNavigate()
 
   const handleLogout = () => {
     logout()
     navigate('/')
+  }
+
+  const handleEdit = () => {
+    if (editable) {
+      editarPerfil(medico)
+    }
+    setEditable(!editable)
   }
 
   useEffect(() => {
@@ -34,14 +42,20 @@ const Perfil = () => {
 
   return (
     <div className='perfil-container'>
+      <button className='perfil-edit-button' onClick={handleEdit}>
+        {editable ? 'Guardar Cambios' : 'Editar Perfil'}
+      </button>
       <div className='perfil-header'>
         <div className='perfil-avatar-placeholder'></div>
-        <div>
-          <h2 className='perfil-medico-nombre'>
-            {medico.nombre} {medico.apellido}
+        <div className='perfil-avatar-nombreCompleto'>
+          <h2 className='perfil-medico-nombre' contentEditable={editable}>
+            {medico.nombre}
           </h2>
-          <p className='perfil-medico-especialidad'>{medico.especialidad}</p>
+          <h2 className='perfil-medico-apellido' contentEditable={editable}>
+            {medico.apellido}
+          </h2>
         </div>
+        <p className='perfil-medico-especialidad'>{medico.especialidad}</p>
       </div>
       <div className='perfil-body'>
         <div className='perfil-info-item'>
@@ -58,23 +72,21 @@ const Perfil = () => {
         </div>
         <div className='perfil-info-item'>
           <span>📩</span>
-          <p>{medico.email}</p>
+          <p contentEditable={editable}>{medico.email}</p>
         </div>
         <div className='perfil-info-item'>
           <span>📞</span>
-          <p >{medico.nroLinea || 'no hay numero de linea.'}</p>
+          <p>{medico.nroLinea || 'no hay numero de linea.'}</p>
         </div>
         <div className='perfil-info-item'>
-          <img
-            src={WhatsApp}
-            alt='WhatsApp'
-            className='contact-icon'
-          />
+          <img src={WhatsApp} alt='WhatsApp' className='contact-icon' />
           <p>{medico.nroWhatsapp || 'no hay numero de WhatsApp.'}</p>
         </div>
         <div className='perfil-descripcion'>
           <h4>Descripción</h4>
-          <p>{medico.descripcion || 'Este médico aún no agregó una descripción.'}</p>
+          <p contentEditable={editable}>
+            {medico.descripcion || 'Este médico aún no agregó una descripción.'}
+          </p>
           <button className='perfil-logout-button' onClick={handleLogout}>
             Cerrar Sesión
           </button>
