61ba756c68a992c85f09e779127e397e5a4546d3
Benja <benjamindominguezfdz@gmail.com>
2025-05-26 17:25:37 -0300

    Fixed validacion matricula

--- backend/src/main/java/com/conectarsalud/backend/service/impl/AuthServiceImpl.java
+++ backend/src/main/java/com/conectarsalud/backend/service/impl/AuthServiceImpl.java
@@ -56,7 +56,7 @@ public class AuthServiceImpl implements AuthService {
                 .ifPresent(existingUsuario -> {
                     throw new UsuarioYaExistenteException();
                 });
-        if (!medico.getMatriculaProfesional().startsWith("MP-\\d+")){
+        if (!medico.getMatriculaProfesional().matches("^MP-\\d+$")) {
             throw new MatriculaInvalidaException("La matricula debe empezar con 'MP-'  seguida de numeros");
         }
 
